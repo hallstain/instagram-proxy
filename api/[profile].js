709fc8cb1b049d6636abc3ab5a90d59b15be662f
Konstantin Kurganov <kurganovk@gmail.com>
2022-01-19 19:10:48 +0300

    debug build

--- api/[profile].js
+++ api/[profile].js
@@ -59,7 +59,7 @@ const handler = async (req, res) => {
   //   })
   // })
   // res.end(JSON.stringify(view))
-  res.end(JSON.stringify(result.json()))
+  res.end(JSON.stringify(result.data))
 }
 
 module.exports = allowCors(handler)
